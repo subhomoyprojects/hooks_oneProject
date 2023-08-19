aca5042db3ca95a67ebc91d08a0879ebb09b01ba
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-08-19 10:59:10 +0530

    add use effect

---
+++ src/UseEffect.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function UseEffect() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array to ensure cleanup is called only once
+
+  return (
+    <div>
+      <h1>
+        My Window Screen Size is <span style={{ color: "#f00" }}>{screenSize}</span>
+      </h1>
+    </div>
+  );
+}
